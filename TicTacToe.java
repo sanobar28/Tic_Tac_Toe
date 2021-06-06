6d7c9b656de8e76d8b396f504a4d274c39e2380b
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-06 15:24:19 +0530

    added: method to creat and print board

---
+++ TicTacToe.java
@@ -0,0 +1,30 @@
+package com;
+
+import java.util.*;
+
+public class TicTacToe {
+	
+	static String[] board;
+    
+
+	/**
+	 * @ UC-1 to create and print board
+	 */
+    static void printBoard()
+    {
+        System.out.println("|---|---|---|");
+        System.out.println("| " + board[0] + " | "
+                           + board[1] + " | " + board[2]
+                           + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[3] + " | "
+                           + board[4] + " | " + board[5]
+                           + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[6] + " | "
+                           + board[7] + " | " + board[8]
+                           + " |");
+        System.out.println("|---|---|---|");
+    }
+  
+}
