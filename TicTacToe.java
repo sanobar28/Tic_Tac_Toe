56671abd8ecfc2000e7cebcf368a5c814dbb940e
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-06 15:38:26 +0530

    added: to choose symbol to play

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,28 +3,45 @@ package com;
 import java.util.*;
 
 public class TicTacToe {
-	
+
 	static String[] board;
-    
+	static String turn;
 
 	/**
 	 * @ UC-1 to create and print board
 	 */
-    static void printBoard()
-    {
-        System.out.println("|---|---|---|");
-        System.out.println("| " + board[0] + " | "
-                           + board[1] + " | " + board[2]
-                           + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + board[3] + " | "
-                           + board[4] + " | " + board[5]
-                           + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + board[6] + " | "
-                           + board[7] + " | " + board[8]
-                           + " |");
-        System.out.println("|---|---|---|");
-    }
-  
+	static void printBoard() {
+		System.out.println("|---|---|---|");
+		System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
+		System.out.println("|---|---|---|");
+	}
+	
+	
+	/**
+	 * Decide who will play first
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		board = new String[9];
+		turn = "X";
+		String winner = null;
+
+		for (int a = 0; a < 9; a++) {
+			board[a] = String.valueOf(a + 1);
+		}
+
+		System.out.println("Welcome to Tic Tac Toe Game.");
+		printBoard();
+
+		System.out.println("X will play first. Enter a slot number to place X in:");
+
+	}
+
 }
